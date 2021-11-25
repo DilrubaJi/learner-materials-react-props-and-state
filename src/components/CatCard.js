faf8161c5badc350e5053336d91c839c029b70e2
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-11-25 23:07:38 +0000

    Activity_6

---
+++ src/components/CatCard.js
@@ -0,0 +1,14 @@
+function CatCard(props) {
+    console.log("CatCard receiving props from App?! Comein App?! Props received are: ", props)
+    return (
+        <div className="card">
+            <h3 className="card__text card__header">{props.name}</h3>
+            <img className="card__image" src={props.photo} alt={props.alt}></img>
+            <p className="card__text">Species: {props.species}</p>
+            <p className="card__text">Favourite Food(s): {props.favFoods}</p>
+            <p className="card__text">Birth Year: {props.birthYear}</p>
+        </div>
+    )
+}
+
+export default CatCard;
