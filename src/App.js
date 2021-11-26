78789dfa0b08f4f696f64aab13545b791cd7d120
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-11-26 00:57:23 +0000

    Activity_8 Ext-1, dogs added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { useState } from 'react'
 import CatCard from './components/CatCard'
+import { v4 as uuidv4 } from 'uuid';
+import DogCard from './components/DogCard';
 
 function App() {
 
@@ -146,20 +148,54 @@ function App() {
     }
   ])
 
+  const [dogs, setDogs] = useState([
+    {
+      "name": "Floofus",
+      "species": "Puppy",
+      "favFoods": ["marshmallows"],
+      "birthYear": 2020,
+      "photo": "https://www.thesprucepets.com/thmb/sfuyyLvyUx636_Oq3Fw5_mt-PIc=/3760x2820/smart/filters:no_upscale()/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg",
+      "alt": "The fluffiest white puppy you've ever seen"
+    },
+    {
+      "name": "Doofus",
+      "species": "Doggo",
+      "favFoods": ["porridge, dog food"],
+      "birthYear": 2010,
+      "photo": "https://hashtaglegend.com/wp-content/uploads/2020/08/international-dog-day-1024x1024.jpg",
+      "alt": "Doggo running through a field with it's tongue out"
+    },
+    {
+      "name": "Goofus",
+      "species": "Dog",
+      "favFoods": ["bouncy balls"],
+      "birthYear": 2015,
+      "photo": "https://i.ytimg.com/vi/EFJwJpAJbDA/maxresdefault.jpg",
+      "alt": "Doggo with too many balls in it's mouth"
+    },
+  ])
+
+
+  const dogCount = dogs.length
   const catCount = cats.length
   console.log("Our pretties: ", cats)
-  console.log("Number of cats= ", catCount)
+  console.log("Number of cats: ", catCount)
+  console.log("Number of Dogs: ", dogCount)
 
   return (
     <>
       <Navbar />
-      <Header catCount={catCount} />
+      <Header catCount={catCount} dogCount={dogCount} />
 
       <main>
         <div className="cards__wrapper">
 
           {cats.map(cat => {
-            return <CatCard name={cat.name} species={cat.species} favFoods={cat.favFoods} birthYear={cat.birthYear} photo={cat.photo} alt={cat.alt} />
+            return <CatCard key={cat.id} name={cat.name} species={cat.species} favFoods={cat.favFoods} birthYear={cat.birthYear} photo={cat.photo} alt={cat.alt} />
+          })}
+
+          {dogs.map(dog => {
+            return <DogCard key={dog.id} name={dog.name} species={dog.species} favFoods={dog.favFoods} birthYear={dog.birthYear} photo={dog.photo} alt={dog.alt} />
           })}
 
         </div>
